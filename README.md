8c00437efd6a2de9edbfe3e0a7eaaf83a2883be6
Raul Cuza <raulcuza@gmail.com>
2016-02-29 15:40:14 -0500

    Improve automation and docs

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ Class repo for https://github.com/suneel0101/python-and-pandas-bootcamp
 course.
 
 
+# Notes
+If you use a guest that does not have VirtualBox guest additions
+installed, like debian/jessie64, you may need to run the following once:
+
+```
+vagrant up; vagrant ssh -c 'sudo ln -s /opt/VBoxGuestAdditions-4.3.10/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions'; vagrant reload
+```
+
+I switch to ubuntu/trusty64 and this became unnecessary.
--- box/bootstrap.sh
+++ box/bootstrap.sh
@@ -5,6 +5,8 @@ die() { yell "$*"; exit 111; }
 try() { "$@" || die "cannot $*"; }
 
 apt-get update
+apt-get -y upgrade
+apt-get -y autoremove
 
 anaconda2="/vagrant/Anaconda2-2.5.0-Linux-x86_64.sh"
 anaconda2url="https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda2-2.5.0-Linux-x86_64.sh"
@@ -15,14 +17,15 @@ if [ ! -e ${anaconda2} ]
 then
   try curl ${anaconda2url} -o ${anaconda2}
 fi
-${anaconda2} -b
+${anaconda2} -b -p "/vagrant/anaconda2"
 
+echo "PATH=\"/vagrant/anaconda2/bin:\$PATH\"" >>/home/vagrant/.bashrc
 
 #if [ ! -e ${anaconda3} ]
 #then
 #  try curl ${anaconda3url} -o ${anaconda3}
 #fi
-#${anaconda3} -b
-
+#${anaconda3} -b -p "/vagrant/anaconda3"
+#echo PATH=\"/vagrant/anaconda3/bin:\$PATH\"" >>/home/vagrant/.bashrc
 
 true
